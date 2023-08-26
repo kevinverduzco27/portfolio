8e0b2fc2dc7c937ce4eac61442bddb5b685bebba
kevinverduzco27 <kevin.verduzco27@gmail.com>
2023-08-25 20:36:23 -0700

    css

---
+++ my-portfolio/src/pages/AboutMe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutMe.css';
+
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <h1>About Me</h1>
+      <p>
+        My name is Kevin, the oldest of three brothers, passionate about programming and technology.
+        {/* More content */}
+      </p>
+    </div>
+  );
+}
+
+export default AboutMe;
